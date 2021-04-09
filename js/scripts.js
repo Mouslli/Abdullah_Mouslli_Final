7c0043545d8c0b20d5ab9578f909c372a0d79877
Mouslli <abdullah.mouslli@gmail.com>
2021-04-09 17:42:20 -0600

    add face mask

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
+
+
 var vidcapture, ctracker, drawcanvas;
+
+
 
 function setup() {
 
-	var cnv = createCanvas (windowWidth, windowHeight/2);
+	var cnv = createCanvas (windowWidth, windowHeight);
 	cnv.parent("p5canvas");
 
 	vidcapture = createCapture (VIDEO);
-	vidcapture.size(vidcapture.width*2, vidcapture.height*2.5);
+	vidcapture.size(vidcapture.width*1.8, vidcapture.height*2.65);
+	vidcapture.hide();
 
 	ctracker = new clm.tracker();
 	ctracker.init ();
 	ctracker.start (vidcapture.elt);
 	drawcanvas = document.getElementById ('defaultCanvas0')
+
+
 
 
 }
@@ -23,32 +30,43 @@ function draw(){
 
 	translate (vidcapture.width, 0)
 	scale (-1, 1)
-	image (vidcapture, 0,0);
+	// image (vidcapture, 0,0);
+
 
 	var position = ctracker.getCurrentPosition ();
 
 	if (position) {
 
-		ctracker.draw(drawcanvas);
-		// fill (0);
-		// ellipse (position[52][0], position [52][1],10);
+		// ctracker.draw(drawcanvas);
+		background(255, 50);
 
-		// var r = map(position[62][0], 250, 300, 0, 255, true)
-		// var b = map(position[62][1], 150, 200, 0, 255, true)
+		var facecenterX= position[33][0];
+		var facecenterY= position[33][1];
 
-		// background (r, 255, b)
-		// tint (255, 126)
-		// image (vidcapture, 0,0)
+		var facewidth = position [14][0] - position [0][0];
+		var faceheight = (position [7][1] - position [0][1])*1.8;
+		
 
-	}
-
-
-
-
-
-
+		var r = map(position[62][0], 150, 300, 0, 255, true)
+		var b = map(position[62][1], 100, 250, 0, 255, true)
+		fill (r,200, b, 100);
+		noStroke();
+		ellipse (facecenterX, facecenterY, facewidth, faceheight);
 
 
 }
+}
 
 
+		// var r = map(position[63][0], 150, 300, 0, 100, true)
+		// var b = map(position[63][1], 100, 250, 0, 100, true)
+		// noStroke();
+		// fill (r, 255, b);
+		// position.forEach (function (pos){
+		// 		ellipse(pos[0], pos[1],7);
+
+
+
+
+
+		// })
