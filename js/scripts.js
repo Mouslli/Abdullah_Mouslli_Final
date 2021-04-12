e46846ac7d0ce020625a64fc565c51ecda1173d2
Mouslli <abdullah.mouslli@gmail.com>
2021-04-12 12:07:10 -0600

    adding mouth?

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var vidcapture, ctracker, drawcanvas;
 
 function setup() {
 
-	var cnv = createCanvas (windowWidth, windowHeight);
+	var cnv = createCanvas (windowWidth/2, windowHeight);
 	cnv.parent("p5canvas");
 
 	vidcapture = createCapture (VIDEO);
@@ -19,10 +19,6 @@ function setup() {
 
 
 }
-
-
-
-
 
 
 function draw(){
@@ -56,6 +52,7 @@ function draw(){
 //map the RGB values of the face with according to the position of the nose 
 		var r1 = map(position[32][0], 100, 300, 0, 255, true)
 		var b1 = map(position[32][1], 100, 350, 0, 255, true)
+
 		fill (r1,200, b1, 100);
 		noStroke();
 		ellipse (position [27][0], position[27][1], 20)
@@ -88,32 +85,42 @@ function draw(){
 		line (position[13][0], position[13][1], position[13][0] , position[13][1]+25);
 		line (position[14][0], position[14][1], position[14][0] , position[14][1]+25);
 
-////Rainbow gif
+////Switch eyes to squares when mouth is open
 		let mouthX = position [60][1]-position[57][1];
 		
 		if (mouthX > mouthX*0.1){ 
 
 
 			noStroke();
-			rect (position [27][0] -15 , position[27][1]- 15, 30, 30)
-			rect (position [32][0] - 15, position[32][1]- 15, 30, 30)
+			fill (r1, 40, b1);
+			rect (mouseX + (position [27][0]-position[32][0]), position[27][1]- 15, 30, 30)
+			rect (mouseX, position[32][1]- 15, 30, 30)
 
 		}
+////Switching between a rectangle and triangle for mouth if the x coordinates of point 62 passes the width mid point
+		if (position [62][0] < width/2){
+
+			noStroke();
+			fill (r, 146, b)
+			rect (position [60][0] -25, position [60][1] -10, 50, 10);
+		}
+
+		if (position [62][0] > width/2) {
+
+			noStroke();
+			fill(r, 146, b)
+			triangle (position [47][0], position[47][1] -20, position [44][0], position[44][1], position[50][0], position[50][1])
+		}
+
+////adding ears if the y coordinates of point 33 is bigger than the quarter the window height
+		if (position[33][1] > height/4) {
+
+			noStroke();
+			ellipse (position[13][0], position[13][1], 30);
+			ellipse (position [1][0], position [1][1], 30);
+		} 
+
 
 
 	}
 }
-
-
-		// var r = map(position[63][0], 150, 300, 0, 100, true)
-		// var b = map(position[63][1], 100, 250, 0, 100, true)
-		// noStroke();
-		// fill (r, 255, b);
-		// position.forEach (function (pos){
-		// 		ellipse(pos[0], pos[1],7);
-
-
-
-
-
-		// })
